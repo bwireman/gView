8107e06a22b5d6180124063b7896214b8708e648
Benjamin Wireman <bwireman@umich.edu>
2018-03-23 22:12:59 -0400

    more work on differentiating branches

--- parser.js
+++ parser.js
@@ -11,7 +11,7 @@ module.exports = class parser {
         
         let fullLogJson = null;
         try {
-            fullLogJson = await git(workingDir).log({'--all': null, '--graph':null, '--decorate':null});
+            fullLogJson = await git(workingDir).log({'--graph':null, '--decorate':null, '--all': null});
         }
         catch (e) {
             console.log(e);
@@ -20,17 +20,54 @@ module.exports = class parser {
         return fullLogJson;
     }
 
+    async getCurrentBranch (workingDir) {
+        const git = require('simple-git/promise');
+        
+        let branch = null;
+        try {
+            branch = await git(workingDir).branch();
+        }
+        catch (e) {
+            console.log(e);
+        }
+        
+        return branch;
+    }
+
     async buildNodes() {
 
         let nodes = [];
 
         let log = await this.log(directoryPath);
-        console.log(log);
         for (var i = 0; i < log.all.length; i++) {
-            nodes.push(new Node (log.all[i].author_name, log.all[i].date, log.all[i].message, log.all[i].author_email));
+            nodes.push(new Node (log.all[i].author_name, log.all[i].date, this.parseBranch(log.all[i].message), log.all[i].message));
+        }
+        let CurrBranch = (await this.getCurrentBranch(directoryPath)).current;
+
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i].Branch == null)
+            {
+                nodes[i].Branch = CurrBranch;
+            }
         }
 
         console.log(nodes);
+        return nodes;
+    }
+
+    parseBranch(commitMessage) {
+
+        if (commitMessage.includes("(") && commitMessage.includes(")"))
+        {
+            let leftParen = commitMessage.indexOf("(");
+            let rightParen = commitMessage.indexOf(")");
+            let temp = commitMessage.substring(leftParen, rightParen).split(",");
+            return temp;
+        }
+        else
+        {
+            return null;
+        }
     }
 
 }
